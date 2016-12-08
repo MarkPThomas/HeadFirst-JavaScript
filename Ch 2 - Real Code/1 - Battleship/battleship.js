a4abc3b9d697b548da73c25741d83a119f0f79e0
MarkPThomas <markums@gmail.com>
2016-12-07 20:17:39 -0800

    Completed Ch. 2, Reorganizde Ch. 1

---
+++ Ch 2 - Real Code/1 - Battleship/battleship.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Mark on 12/7/2016.
+ */
+
+var randomLoc = Math.floor(Math.random()*5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while (isSunk == false)
+{
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
+    if (guess < 0 || guess > 6)
+    {
+        alert("Please enter a valid cell number!");
+    }
+    else
+    {
+        guesses += 1;
+        if (guess == location1 ||
+            guess == location2 ||
+            guess == location3)
+        {
+            hits += 1;
+            alert("HIT!");
+            if (hits == 3)
+            {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }
+        else
+        {
+            alert("MISS!");
+        }
+    }
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+        "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
